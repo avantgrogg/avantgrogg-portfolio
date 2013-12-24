48a4b74cdad2e674c33537bae06fadb9ea64eec1
Doug Grogg <thegroggman@gmail.com>
2013-12-23 19:11:02 -0500

    updated site name css

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 var stickyTop = $(".thirty").offset().top*.5;
-  $(window).scroll(function(){ // scroll event  
- 
+$(window).scroll(function(){ // scroll event  
+
     var windowTop = $(window).scrollTop(); // returns number
  
     if (stickyTop < windowTop) {
@@ -9,4 +9,12 @@ var stickyTop = $(".thirty").offset().top*.5;
     else {
       $('.show-text').fadeOut('slow');
     }
+});
+
+$(".show-button").click(function(event){
+    $(".show-button").html("doug@avantgrogg.com");   
+});
+
+$("#jumbo .blue").click(function(event){
+   $("#jumbo .blue").html("doug@avantgrogg.com"); 
 });
